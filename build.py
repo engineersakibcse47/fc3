2c9dcb48f36e9be6a4a7cbe85a5b2e9f3b44786d
Sakib Hosen Himel <108215990+engineersakibcse47@users.noreply.github.com>
2022-11-26 16:47:04 +0600

    Add files via upload

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 use_plugin('python.pycharm')
-use_plugin("python.install_dependencies")
+
 
 name = "Face Recognition"
 default_task = "publish"
@@ -15,5 +15,4 @@ default_task = "publish"
 
 @init
 def set_properties(project):
-    project.depends_on_requirements("requirement.txt")
     project.set_property("coverage_break_build", False)
